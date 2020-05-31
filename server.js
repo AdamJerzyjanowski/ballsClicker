6a85e3ff954279759f9668efc13d8f7a6d762832
AdamJerzyJanowski <AdamJerzyJanowski@gmail.com>
2020-05-31 16:03:26 +0200

    2.5 cors is comment

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var cors = require('cors');
+//var cors = require('cors');
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse requests of content-type - application/json
